0d7aa7c3341e624ff971a62919b5bd2066e7b033
KevinOmega365 <kevin@omega365.com>
2023-11-24 11:30:57 +0100

    Update PositiveIntegrationCounts.sql

--- src/dcs_integration/PositiveIntegrationCounts.sql
+++ src/dcs_integration/PositiveIntegrationCounts.sql
@@ -2,43 +2,44 @@
  * Positive integration counts
  */
 
-/**
- * Count document creation
- */
-select count(*)
-from dbo.atbl_DCS_Documents D with (nolock)
-where
-    CreatedBy like 'af_Integrations_ServiceUser'
-    and Created > dateadd(week, -1, getdate())
-    and Domain = '181'
+ DECLARE @Domain nvarchar(128) = '181'
 
-/**
- * Count document updates
- */
-select count(*)
-from dbo.atbl_DCS_Documents D with (nolock)
-where
-    Created < dateadd(week, -1, getdate())
-    and UpdatedBy like 'af_Integrations_ServiceUser'
-    and Updated > dateadd(week, -1, getdate())
-    and Domain = '181'
+select
+[count_document_creation] =
+(
+    select count(*)
+    from dbo.atbl_DCS_Documents D with (nolock)
+    where
+        CreatedBy like 'af_Integrations_ServiceUser'
+        and Created > dateadd(week, -1, getdate())
+        and Domain = @Domain
+)
+,[count_document_updates] =
+(
+    select count(*)
+    from dbo.atbl_DCS_Documents D with (nolock)
+    where
+        Created < dateadd(week, -1, getdate())
+        and UpdatedBy like 'af_Integrations_ServiceUser'
+        and Updated > dateadd(week, -1, getdate())
+        and Domain = @Domain
+)
+,[count_revision_creation] =
+(
+    select count(*)
+    from dbo.atbl_DCS_Revisions R with (nolock)
+    where
+        CreatedBy like 'af_Integrations_ServiceUser'
+        and Created > dateadd(week, -1, getdate())
+        and Domain = @Domain
+)
 
-/**
- * Count revision creation
- */
-select count(*)
-from dbo.atbl_DCS_Revisions R with (nolock)
-where
-    CreatedBy like 'af_Integrations_ServiceUser'
-    and Created > dateadd(week, -1, getdate())
-    and Domain = '181'
-
-/**
- * Count revision file creation
- */
-select count(*)
-from dbo.atbl_DCS_RevisionsFiles RF with (nolock)
-where
-    CreatedBy like 'af_Integrations_ServiceUser'
-    and Created > dateadd(week, -1, getdate())
-    and Domain = '181'
+,[count_revision_file_creation] =
+(
+    select count(*)
+    from dbo.atbl_DCS_RevisionsFiles RF with (nolock)
+    where
+        CreatedBy like 'af_Integrations_ServiceUser'
+        and Created > dateadd(week, -1, getdate())
+        and Domain = @Domain
+)
