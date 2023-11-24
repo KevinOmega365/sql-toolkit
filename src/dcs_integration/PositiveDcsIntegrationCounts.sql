2f5fe25f5789ee489898fc0a15d9ed0bf517af6e
KevinOmega365 <kevin@omega365.com>
2023-11-24 11:51:33 +0100

    Update PositiveDcsIntegrationCounts.sql

--- src/dcs_integration/PositiveDcsIntegrationCounts.sql
+++ src/dcs_integration/PositiveDcsIntegrationCounts.sql
@@ -5,7 +5,7 @@
 declare @Domain nvarchar(128) = '181'
 
 select
-    [count_document_creation] =
+    DocumentsCreated =
     (
         select count(*)
         from dbo.atbl_DCS_Documents D with (nolock)
@@ -14,17 +14,17 @@ select
             and Created > dateadd(week, -1, getdate())
             and Domain = @Domain
     ),
-    [count_document_updates] =
+    DocumentsUpdated =
     (
         select count(*)
         from dbo.atbl_DCS_Documents D with (nolock)
         where
-            Created < dateadd(week, -1, getdate())
+            datediff(minute, Created, Updated) > 1
             and UpdatedBy like 'af_Integrations_ServiceUser'
             and Updated > dateadd(week, -1, getdate())
             and Domain = @Domain
     ),
-    [count_revision_creation] =
+    RevisionsCreated =
     (
         select count(*)
         from dbo.atbl_DCS_Revisions R with (nolock)
@@ -33,7 +33,7 @@ select
             and Created > dateadd(week, -1, getdate())
             and Domain = @Domain
     ),
-    [count_revision_file_creation] =
+    RevisionFilesCreated =
     (
         select count(*)
         from dbo.atbl_DCS_RevisionsFiles RF with (nolock)
