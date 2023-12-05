a74a91bc8704c53db29b0e9504a330dae861c5a8
Kevin Becker <97727548+KevinOmega365@users.noreply.github.com>
2023-12-05 09:41:22 +0100

    Clean up and unify Check Document Existence SQL

--- src/dcs_integration/CheckAksoDocumentImport.sql
+++ src/dcs_integration/CheckAksoDocumentImport.sql
@@ -9,39 +9,29 @@ insert into @QuestionDocuments
 values
     ('YOUR_DOCUMENT_NUMBER', 'YOUR_REVISION') --, ...
 
-select * from
+select distinct *
+from
 (
-    select
+    select -- distinct
+        ImportDomain = ID.DCS_Domain,   
         Q.DocumentID,
         Q.Revision,
         ImportDocumentExists = case when ID.PrimKey is null then 0 else 1 end,
-        ImportDocumentStatus = ID.INTEGR_REC_STATUS,
         ImportRevisionExists = case when IR.PrimKey is null then 0 else 1 end,
-        ImportRevisionStatus = IR.INTEGR_REC_STATUS,
         PimsDocumentExists = case when PD.PrimKey is null then 0 else 1 end,
         PimsRevisionExists = case when PR.PrimKey is null then 0 else 1 end
     from
         @QuestionDocuments Q -- hang everything off Q
-        left join dbo.ltbl_Import_MuninAibel_Documents as ID with (nolock)
-            on ID.DCS_DocumentID = Q.DocumentID
-        left join dbo.ltbl_Import_MuninAibel_Revisions as IR with (nolock)
-            on IR.DCS_DocumentID = Q.DocumentID
-            and IR.DCS_Revision = Q.Revision
+        left join dbo.ltbl_Import_ProArc_Documents as ID with (nolock)
+            on ID.document_number = Q.DocumentID
+        left join dbo.ltbl_Import_ProArc_Revisions as IR with (nolock)
+            on IR.document_number = Q.DocumentID
+            and IR.revision = Q.Revision
         left join dbo.atbl_DCS_Documents as PD with (nolock)
             on PD.DocumentID = Q.DocumentID
         left join dbo.atbl_DCS_Revisions as PR with (nolock)
             on PR.DocumentID = Q.DocumentID
             and PR.Revision = Q.Revision
-    where
-        (
-            PD.Domain is null
-            or PD.Domain = '181'
-        )
-        and
-        (
-            PR.Domain is null
-            or PD.Domain = '181'
-        )
 ) T
 where
     ImportDocumentExists = 0
