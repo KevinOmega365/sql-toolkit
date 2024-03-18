bcdb7b9ce95ea64e4aa1bd628a55f86e28c68fe7
Kevin Becker <kevinfjbecker@gmail.com>
2024-03-18 13:41:25 +0100

    Create MappingSetToJson.sql

---
+++ src/adHoc/MappingSetToJson.sql
@@ -0,0 +1,43 @@
+declare @MappingSetID nvarchar(50) = N'UPP DTS - DCS Documents Renaming'
+
+select
+    [MappingSetID],
+    [Description],
+    [FieldMappingSets] = (
+        select
+            [MappingSetID],
+            [MappingSetValueID],
+            [CriteriaValue1],
+            [CriteriaValue2],
+            [FromValue],
+            [ToValue]
+        from
+            [dbo].[atbl_Integrations_Configurations_FieldMappingSets_Values] with (nolock)
+        where
+            [MappingSetID] = @MappingSetID
+        for
+            json auto
+    ),
+    [Subscribers] = (
+        select
+            [MappingSetID],
+            [GroupRef],
+            [TargetTable],
+            [CriteriaField1],
+            [CriteriaField2],
+            [FromField],
+            [ToField],
+            [Required]
+        from
+            [dbo].[atbl_Integrations_Configurations_FieldMappingSets_Subscribers] with (nolock)
+        where
+            [MappingSetID] = @MappingSetID
+        for
+            json auto
+    )
+from
+    [dbo].[atbl_Integrations_Configurations_FieldMappingSets] with (nolock)
+where
+    [MappingSetID] = @MappingSetID
+for
+    json auto
