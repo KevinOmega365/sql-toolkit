34705081999c6aa75d8052b24fd49c861ed4215d
Kevin Becker <97727548+KevinOmega365@users.noreply.github.com>
2024-05-13 10:28:46 +0200

    Delete obsolete versions of import compare

--- src/reporting/dts_fdm_files.sql
+++
@@ -1,6 +0,0 @@
-declare @dts_ygg_groupRef uniqueidentifier = 'efd3449e-3a44-4c38-b0e7-f57ca48cf8b0'
-declare @fdm_ygg_groupRef uniqueidentifier = '5efd7e52-e187-491c-a9cc-1f8f97eebb70'
-
-    select System = 'DTS', RevisionCount = count(*) from dbo.ltbl_Import_DTS_DCS_RevisionsFiles with (nolock) where INTEGR_REC_GROUPREF = @dts_ygg_groupRef
-union all
-    select System = 'FDM', RevisionCount = count(*) from dbo.ltbl_Import_ProArc_RevisionFiles with (nolock) where INTEGR_REC_GROUPREF = @fdm_ygg_groupRef
