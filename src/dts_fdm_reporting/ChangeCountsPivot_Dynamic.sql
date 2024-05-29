dd59d26134a46482dcbe8c95212a2e94b08d6312
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-05-29 14:13:53 +0200

    Update ChangeCountsPivot_Dynamic.sql

--- src/dts_fdm_reporting/ChangeCountsPivot_Dynamic.sql
+++ src/dts_fdm_reporting/ChangeCountsPivot_Dynamic.sql
@@ -14,22 +14,35 @@ declare
         { "sourceColumn" : "DCS_DocumentID", "destinationColumn" : "DocumentID" }
     ]',
     @comparisonColumns nvarchar(max) = '[
-        { "sourceColumn" : "DCS_Title", "destinationColumn" : "Title" },
-        { "sourceColumn" : "DCS_PlantID", "destinationColumn" : "PlantID" },
-        { "sourceColumn" : "DCS_OriginatorCompany", "destinationColumn" : "OriginatorCompany" },
-        { "sourceColumn" : "DCS_FacilityID", "destinationColumn" : "FacilityID" },
-        { "sourceColumn" : "DCS_DocumentType", "destinationColumn" : "DocumentType" },
-        { "sourceColumn" : "DCS_DocumentGroup", "destinationColumn" : "DocumentGroup" },
-        { "sourceColumn" : "DCS_Discipline", "destinationColumn" : "Discipline" },
+        { "sourceColumn" : "DCS_Area", "destinationColumn" : "Area" },
+        { "sourceColumn" : "DCS_Classification", "destinationColumn" : "Classification" },
+        { "sourceColumn" : "DCS_Confidential", "destinationColumn" : "Confidential" },
         { "sourceColumn" : "DCS_ContractNo", "destinationColumn" : "ContractNo" },
         { "sourceColumn" : "DCS_Criticality", "destinationColumn" : "Criticality" },
-        { "sourceColumn" : "DCS_Area", "destinationColumn" : "Area" },
-        { "sourceColumn" : "DCS_Confidential", "destinationColumn" : "Confidential" },
-        { "sourceColumn" : "DCS_Classification", "destinationColumn" : "Classification" },
+        { "sourceColumn" : "DCS_DFO", "destinationColumn" : "DFO" },
+        { "sourceColumn" : "DCS_Discipline", "destinationColumn" : "Discipline" },
+        { "sourceColumn" : "DCS_DocumentGroup", "destinationColumn" : "DocumentGroup" },
+        { "sourceColumn" : "DCS_DocumentType", "destinationColumn" : "DocumentType" },
+        { "sourceColumn" : "DCS_FacilityID", "destinationColumn" : "FacilityID" },
+        { "sourceColumn" : "DCS_OriginatorCompany", "destinationColumn" : "OriginatorCompany" },
+        { "sourceColumn" : "DCS_PlantID", "destinationColumn" : "PlantID" },
         { "sourceColumn" : "DCS_PONumber", "destinationColumn" : "PONumber" },
+        { "sourceColumn" : "DCS_ReviewClass", "destinationColumn" : "ReviewClass" },
         { "sourceColumn" : "DCS_System", "destinationColumn" : "System" },
+        { "sourceColumn" : "DCS_Title", "destinationColumn" : "Title" },
         { "sourceColumn" : "DCS_VoidedDate", "destinationColumn" : "VoidedDate" },
-        { "sourceColumn" : "DCS_ReviewClass", "destinationColumn" : "ReviewClass" }
+
+        { "sourceColumn" : "DCS_ContractorDocumentID", "destinationColumn" : "ContractorDocumentID" },
+        
+        { "sourceColumn" : "DCS_AssetCustomText1", "destinationColumn" : "AssetCustomText1" },
+        { "sourceColumn" : "DCS_DocsCustomFreeText1", "destinationColumn" : "DocsCustomFreeText1" },
+        { "sourceColumn" : "DCS_DocsCustomText1", "destinationColumn" : "DocsCustomText1" },
+        { "sourceColumn" : "DCS_DocsCustomText2", "destinationColumn" : "DocsCustomText2" },
+        { "sourceColumn" : "DCS_DocsCustomText3", "destinationColumn" : "DocsCustomText3" },
+        { "sourceColumn" : "DCS_DocsCustomText4", "destinationColumn" : "DocsCustomText4" },
+        { "sourceColumn" : "DCS_Flag", "destinationColumn" : "Flag" },
+        { "sourceColumn" : "DCS_InstanceCustomText2", "destinationColumn" : "InstanceCustomText2" },
+        { "sourceColumn" : "DCS_InstanceCustomText3", "destinationColumn" : "InstanceCustomText3" }
     ]'
 
 -------------------------------------------------------------------------------
