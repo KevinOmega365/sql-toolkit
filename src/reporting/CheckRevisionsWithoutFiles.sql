bd92d06a145aa34bc61fa03949db900782e5bb63
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-08-01 15:38:46 +0200

    Dashboard data to play with and more info for missing files

--- src/reporting/CheckRevisionsWithoutFiles.sql
+++ src/reporting/CheckRevisionsWithoutFiles.sql
@@ -7,6 +7,12 @@ select distinct
     R.DCS_Domain,
     R.DCS_DocumentID,
     R.DCS_Revision,
+    DocIsInPims =
+        case
+            when D.PrimKey is null
+            then 'nope'
+            else 'yupp'
+        end,
     FileMetadata =
         case
             when RF.PrimKey is null
@@ -27,6 +33,7 @@ select distinct
         end
 from
     dbo.ltbl_Import_DTS_DCS_Revisions R with (nolock)
+
     left join dbo.ltbl_Import_DTS_DCS_RevisionsFiles RF with (nolock)
         on RF.DCS_Domain = R.DCS_Domain
         and RF.DCS_DocumentID = R.DCS_DocumentID
@@ -35,6 +42,10 @@ from
         on F.object_guid = RF.object_guid
     left join dbo.stbl_System_Files as SF with (nolock)
         on SF.PrimKey = F.FileRef
+
+    left join dbo.atbl_DCS_Documents D with (nolock)
+        on D.Domain = R.DCS_Domain
+        and D.DocumentID = R.DCS_DocumentID
 where
     R.INTEGR_REC_ERROR = 'Quallity Failure: Revision without Files'
     and R.INTEGR_REC_GROUPREF = @groupRef
