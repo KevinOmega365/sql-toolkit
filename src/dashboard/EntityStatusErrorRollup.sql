d4a469dcfea3951145b994542ec45adb76ed256f
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-08-13 14:11:02 +0200

    Update EntityStatusErrorRollup.sql

--- src/dashboard/EntityStatusErrorRollup.sql
+++ src/dashboard/EntityStatusErrorRollup.sql
@@ -1,4 +1,12 @@
-declare @GroupRef uniqueidentifier = 'edadd424-81ce-4170-b419-12642f80cfde'
+
+declare
+    @IvarAasen uniqueidentifier = 'f6c3687c-5511-48f2-98e5-8e84eee9b689',
+    @Munin uniqueidentifier = 'e1a66f7c-ab9b-4586-aa71-4b4cab743aa2',
+    @Valhall uniqueidentifier = '564d970e-8b1a-4a4a-913b-51e44d4bd8e7',
+    @Yggdrasil uniqueidentifier = 'efd3449e-3a44-4c38-b0e7-f57ca48cf8b0',
+    @EdvardGrieg uniqueidentifier = 'edadd424-81ce-4170-b419-12642f80cfde'
+
+declare @GroupRef uniqueidentifier = @Valhall
 
 /*
  * Entity Status Error Rollup
