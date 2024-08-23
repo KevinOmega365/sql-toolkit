a8fe9882ef0e1f81c47a71077edb7db3f8c1b9e1
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-08-23 13:22:08 +0200

    Update ServiceLogJsonExtract.sql

--- src/adHoc/FileNotFoundErrors/ServiceLogJsonExtract.sql
+++ src/adHoc/FileNotFoundErrors/ServiceLogJsonExtract.sql
@@ -1,26 +1,62 @@
-SELECT top 50
-    PrimKey,
-    Created,
-    CreatedBy,
-    Updated,
-    UpdatedBy,
-    ServiceName,
-    ExecutionGroupRef,
-    ExecutionBatchRef,
-    ExecutionTaskRef,
-    CallingObject,
-    CallingMethod,
-    CallingParameters,
-    Message,
-    JsonExtract = reverse(left(reverse(left(Message, charindex('}', Message))), charindex('{', reverse(left(Message, charindex('}', Message)))))),
-    JsonCheck = isjson(reverse(left(reverse(left(Message, charindex('}', Message))), charindex('{', reverse(left(Message, charindex('}', Message))))))),
-    StackTrace,
-    LogLevel,
-    SortOrder
-FROM
-    dbo.aviw_Integrations_ScheduledTasksServicesLog
-WHERE
-    ExecutionGroupRef = '564d970e-8b1a-4a4a-913b-51e44d4bd8e7'
-    and Message like '%404%'
+/*
+
+Questions:
+
+How often?
+How much?
+How many recurences?
+
+Which 404 error don't have JSON content
+
+How do these corellate with import messages
+
+*/
+
+select
+    LogEntries.Created,
+    LogEntries.ExecutionGroupRef,
+    LogEntries.ExecutionBatchRef,
+    -- MessageJsonContent.md5hash,
+    MessageJsonContent.object_guid,
+    MessageJsonContent.originalFilename,
+    MessageJsonContent.ShortMessage
+    -- LogEntries.Message,
+    -- LogEntries.JsonExtract,
+    -- LogEntries.JsonCheck = isjson(JsonExtract)
+from
+    (
+        SELECT TOP 50
+            Created,
+            ExecutionGroupRef,
+            ExecutionBatchRef,
+            ShortMessage = right(Message, len(Message) - charindex('}', Message) - 3),
+            Message,
+            JsonExtract =
+                reverse(
+                    left(
+                        reverse(
+                            left(
+                                Message,
+                                charindex('}', Message)
+                            )
+                        ),
+                        charindex(
+                            '{',
+                            reverse(left(Message, charindex('}', Message)))
+                        )
+                    )
+                )
+        FROM
+            dbo.atbl_Integrations_ScheduledTasksServicesLog AS LogEntries WITH(NOLOCK)
+        WHERE
+            ExecutionGroupRef = '564d970e-8b1a-4a4a-913b-51e44d4bd8e7'
+            and Message like '%404%'
+    ) LogEntries
+    cross apply openjson(JsonExtract)
+        with (
+            md5hash nvarchar(max),
+            object_guid nvarchar(max),
+            originalFilename nvarchar(max)
+        ) MessageJsonContent
 ORDER BY
     Created DESC
