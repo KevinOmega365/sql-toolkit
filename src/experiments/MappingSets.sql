826bb4e7c9aff74605c5af037a7562f99c455fe6
Kevin Becker <kevin@omega365.com>
2024-09-16 16:01:29 +0200

    Update MappingSets.sql

--- src/experiments/MappingSets.sql
+++ src/experiments/MappingSets.sql
@@ -1,27 +1,45 @@
 declare @Pipelines table
-{
-    PrimKey
-    Name
-}
+(
+    PrimKey uniqueidentifier,
+    Name nvarchar(128)
+)
 
 declare @PipelinesMappingSets table
-{
-    GroupRef uniqueidentifier,
-    MappingRef uniqueidentifier,
-    Priority int
-}
+(
+    GroupRef uniqueidentifier, -- not null
+    MappingRef uniqueidentifier, -- not null
+    Priority int -- not null: default = 1
+)
 
 declare @MappingSets table
-{
-    PrimKey
-    Name
-    Type
-}
+(
+    PrimKey uniqueidentifier, -- not null
+    Name nvarchar(128), -- not null
+    MappingType nvarchar(128), -- not null: { 'ValueMapping' | 'Renaming' }
+    TableName nvarchar(128) -- not null
+)
 
 declare @ValueMappings table
-{
-    PrimKey
-    KeyValueOne
-    KeyValueTwo
-    KeyValueThree
-}
+(
+    PrimKey uniqueidentifier, -- not null
+    InputValueOne nvarchar(max), -- null
+    InputValueTwo nvarchar(max), -- null
+    InputValueThree nvarchar(max), -- null
+    OutputValue nvarchar(max) -- not null
+)
+
+declare @FieldMappings table
+(
+    PrimKey uniqueidentifier, -- not null
+    InputFieldOne nvarchar(128), -- not null
+    InputFieldTwo nvarchar(128), -- null
+    InputFieldThree nvarchar(128), -- null
+    OutputField nvarchar(128) -- not null
+)
+
+declare @MappingSetsMappings table
+(
+    MappingSetRef uniqueidentifier, -- not null
+    FieldMappingRef uniqueidentifier, -- not null
+    ValueMappingRef uniqueidentifier -- null
+)
