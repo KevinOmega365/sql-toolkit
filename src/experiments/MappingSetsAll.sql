030489cc57e66d5c8ae0280d973e447c335050ff
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-09-18 16:26:22 +0200

    Update MappingSetsAll.sql

--- src/experiments/MappingSetsAll.sql
+++ src/experiments/MappingSetsAll.sql
@@ -1,3 +1,14 @@
+
+DECLARE
+    @IvarAasen UNIQUEIDENTIFIER = 'f6c3687c-5511-48f2-98e5-8e84eee9b689',
+    @Munin UNIQUEIDENTIFIER = 'e1a66f7c-ab9b-4586-aa71-4b4cab743aa2',
+    @Valhall UNIQUEIDENTIFIER = '564d970e-8b1a-4a4a-913b-51e44d4bd8e7',
+    @Yggdrasil UNIQUEIDENTIFIER = 'efd3449e-3a44-4c38-b0e7-f57ca48cf8b0',
+    @Subsea UNIQUEIDENTIFIER = 'fb36536c-db59-4926-952a-5868262a44a5',
+    @EdvardGrieg UNIQUEIDENTIFIER = 'edadd424-81ce-4170-b419-12642f80cfde'
+
+DECLARE @GroupRef NVARCHAR(36) = @Subsea
+
 -------------------------------------------------------------------------------
 ----------------------------------------------- Break into normalized tables --
 -------------------------------------------------------------------------------
@@ -158,7 +169,7 @@ SELECT
         from @MappingSets MS
         where
             MS.Name = GFM.MappingSetID
-        and MS.TableName = GFM.TargetTable
+            and MS.TableName = GFM.TargetTable
     ),
     FieldMappingRef = (
         select Primkey
@@ -180,7 +191,18 @@ SELECT
     )
 FROM
     [dbo].[aviw_Integrations_Configurations_FieldMappingSets_GroupFieldMappings] GFM
-
+GROUP BY
+    MappingSetID,
+    TargetTable,
+    FromField,
+    CriteriaField1,
+    CriteriaField2,
+    ToField,
+    FromValue,
+    CriteriaValue1,
+    CriteriaValue2,
+    ToValue
+    
 -------------------------------------------------------------------------------
 -------------------------------------------------------- Reassemble and Test --
 -------------------------------------------------------------------------------
