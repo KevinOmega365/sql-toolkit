7e73e894e5b65b133eaf4df83b6730763e89d248
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-11-21 14:33:25 +0100

    Update UserImportUI.sql

--- src/adHoc/365/UserImportUI.sql
+++ src/adHoc/365/UserImportUI.sql
@@ -7,9 +7,9 @@ select
     atbl_TGE_AzureAdUsers_Raw = (select count(*) from dbo.atbl_TGE_AzureAdUsers_Raw),
     atbl_TGE_AzureAdUsers_Staging = (select count(*) from dbo.atbl_TGE_AzureAdUsers_Staging)
 
-delete dbo.atbl_TGE_AzureAdGroupsMembers
-delete dbo.atbl_TGE_AzureAdUsers_Raw
-delete dbo.atbl_TGE_AzureAdUsers_Staging
+-- delete dbo.atbl_TGE_AzureAdGroupsMembers
+-- delete dbo.atbl_TGE_AzureAdUsers_Raw
+-- delete dbo.atbl_TGE_AzureAdUsers_Staging
 
 -- insert into dbo.atbl_TGE_AzureAdGroups (Name,Description,AzureID)
 -- values ('AZURE_ZscalerApp_Key_Users','Azure Group for Zscaler Client connector Pilot','ea12262d-e5aa-40f3-81c9-5d9a4c869a5f')
