d3d4196fc07a8ed1139b398713b6306a62d65d3e
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2024-12-04 14:13:31 +0100

    Create SimpleMailMessages.sql

---
+++ src/adHoc/365/SimpleMailMessages.sql
@@ -0,0 +1,32 @@
+-- insert into dbo.stbl_Mail_SimpleMessages(
+--     ForceSending,
+--     FromEmail,
+--     FromName,
+--     ToEmail,
+--     ToName,
+--     Subject,
+--     TextHTML
+-- )
+-- SELECT
+--     ForceSending = 1,
+--     FromMail = 'no-reply@omega365.com',
+--     FromName = 'Omega 365 TGE',
+--     ToEmail = 'kevin@omega365.com',
+--     ToName = 'Kevin Francis',
+--     Subject = 'Test email',
+--     Body =
+-- 'Hi Kevin,
+-- <br>
+-- <br>
+-- There is no action required. This is a test.
+-- <br>
+-- <br>
+-- Have a nice day!
+-- <br>
+-- <br>
+-- Mvh
+-- <br>
+-- &nbsp;&nbsp;&nbsp;&nbsp;Pims
+-- '
+
+select top 50 * from dbo.stbl_Mail_SimpleMessages order by Created desc
