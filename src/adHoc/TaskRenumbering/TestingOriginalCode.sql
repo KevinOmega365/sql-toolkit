97cbdbd00a0d66bc8ee7952e916a4b1ee691e606
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2025-03-14 15:02:15 +0100

    Create TestingOriginalCode.sql

---
+++ src/adHoc/TaskRenumbering/TestingOriginalCode.sql
@@ -0,0 +1,114 @@
+
+DECLARE @TestingPipelinePrimKey UNIQUEIDENTIFIER = '98b779b1-02ef-4886-82ef-19ab350f4ab3'
+
+/*
+ * Proc parameters
+ */
+DECLARE @PipelineStepPrimKey UNIQUEIDENTIFIER = '9fce2e18-b4e9-4f1e-b849-257a560f1763'
+DECLARE @newSequenceOrder NVARCHAR(15) = '5.1.1'
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+declare @MockGroupTasks table
+(
+    PrimKey uniqueidentifier,
+    GroupRef uniqueidentifier,
+    SortOrder int,
+    SequenceOrder nvarchar(15)
+)
+
+insert into @MockGroupTasks
+(
+    PrimKey,
+    GroupRef,
+    SortOrder,
+    SequenceOrder
+)
+select top 50
+    PrimKey,
+    GroupRef,
+    SortOrder,
+    SequenceOrder
+from
+    dbo.atbl_Integrations_ScheduledTasksConfigGroupTasks with (nolock)
+where
+    GroupRef=@TestingPipelinePrimKey
+order by
+    SortOrder
+
+select * 
+into #OriginalMockGroupTasks
+from @MockGroupTasks
+order by SortOrder
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+    DECLARE @newSortOrder INT = 0
+    DECLARE @newSequenceOrderPrefix NVARCHAR(15)
+    DECLARE @newSequenceOrderLastPart NVARCHAR(15)
+
+    DECLARE @GroupRef AS UNIQUEIDENTIFIER;
+
+    SELECT @GroupRef=GroupRef FROM @MockGroupTasks WHERE PrimKey = @PipelineStepPrimKey;
+
+    IF EXISTS (
+        SELECT TOP 1 1 
+        FROM @MockGroupTasks
+        WHERE GroupRef = @GroupRef AND PrimKey <> @PipelineStepPrimKey AND SequenceOrder = @newSequenceOrder
+        ) 
+    BEGIN
+        DECLARE @NumDots INT = 0
+        SET @NumDots = LEN(@newSequenceOrder) - LEN(REPLACE(@newSequenceOrder, '.', ''))
+
+        SELECT @newSortOrder =  dbo.afnc_Integrations_GetSortOrder(@newSequenceOrder)
+
+        SET @newSequenceOrderPrefix = LEFT(@newSequenceOrder,LEN(@newSequenceOrder) - CHARINDEX('.', REVERSE(@newSequenceOrder))+1)
+        SET @newSequenceOrderLastPart = SUBSTRING(@newSequenceOrder, LEN(@newSequenceOrderPrefix)+1, LEN(@newSequenceOrder) - LEN(@newSequenceOrderPrefix)+1)
+
+        IF @NumDots = 0
+        BEGIN
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = '999', SortOrder = dbo.afnc_Integrations_GetSortOrder('999')
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey = @PipelineStepPrimKey;
+
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = SequenceOrder + 1
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey <> @PipelineStepPrimKey AND SortOrder >= @newSortOrder
+
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = @newSequenceOrder
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey = @PipelineStepPrimKey;
+        END
+
+        IF @NumDots > 0
+        BEGIN
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = '9999'
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey = @PipelineStepPrimKey;
+
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = @newSequenceOrderPrefix + CAST(CAST(SUBSTRING(SequenceOrder, LEN(@newSequenceOrderPrefix)+1, LEN(SequenceOrder) - LEN(@newSequenceOrderPrefix)+1) AS INT) + 1 AS NVARCHAR(MAX))
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey <> @PipelineStepPrimKey AND SortOrder >= @newSortOrder AND SequenceOrder LIKE @newSequenceOrderPrefix + '%';
+
+            UPDATE @MockGroupTasks 
+            SET SequenceOrder = @newSequenceOrder
+            FROM @MockGroupTasks
+            WHERE GroupRef = @GroupRef AND PrimKey = @PipelineStepPrimKey 
+        END
+    END
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+select * from #OriginalMockGroupTasks
+select * from @MockGroupTasks
