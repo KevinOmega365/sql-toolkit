4e3f375c645e42ae29d4eb9015e0b5d94d645f36
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2025-05-09 11:18:34 +0200

    Update PastedDocumentRevisions.js

--- src/dcs_integration/PastedDocumentRevisions.js
+++ src/dcs_integration/PastedDocumentRevisions.js
@@ -3,8 +3,12 @@ const pastedDocumentNumbersAndRevisions =
 
 const sqlValuesString = pastedDocumentNumbersAndRevisions
     .split('\n')
-    .filter(x => x) // remove false-y values (e.g., '')
+    .filter((x) => x) // remove empty lines -- false-y values (e.g., '')
     .map((line) => line.split('\t'))
+    .filter((line) => {
+        // remove whitespace lines -- arrays of false-y values (e.g., ['',''])
+        return ! line.every((value) => ! value)
+    })
     .map((values) => {
         if(values[2].length === 1) values[2] = '0' + values[2]
         return values
