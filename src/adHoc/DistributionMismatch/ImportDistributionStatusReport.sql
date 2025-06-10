0ff5e5a4de00a9c48053171e29ade2a6eeb24856
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2025-06-10 13:01:34 +0200

    Update ImportDistributionStatusReport.sql

--- src/adHoc/DistributionMismatch/ImportDistributionStatusReport.sql
+++ src/adHoc/DistributionMismatch/ImportDistributionStatusReport.sql
@@ -1,3 +1,60 @@
+
+-------------------------------------------------------------------------------
+---------------------------------------------- Start: Map Custom Field Names --
+-------------------------------------------------------------------------------
+
+DECLARE @DistributionFlagColumns NVARCHAR(MAX)  = '[
+    "AssetCustomText1",
+    "DocsCustomFreeText1",
+    "DocsCustomText1",
+    "DocsCustomText2",
+    "DocsCustomText3",
+    "DocsCustomText4",
+    "Flag",
+    "InstanceCustomText2",
+    "InstanceCustomText3"
+]'
+
+-------------------------------------------------------------------------------
+
+declare @Domain nvarchar(128) = '128'
+
+declare @FieldNameMapping table
+(
+    ColumnName nvarchar(128),
+    FieldName nvarchar(128)
+)
+
+-------------------------------------------------------------------------------
+
+insert into @FieldNameMapping (
+    ColumnName,
+    FieldName
+)
+select
+    ColumnName = value,
+    FieldName = COALESCE(
+        (
+            SELECT Caption
+            FROM dbo.atbl_DCS_CustomFields with (nolock)
+            WHERE
+                [Domain] = @Domain
+                AND CustomField = value
+        ),
+        (
+            SELECT Caption
+            FROM dbo.atbl_DCS_InstanceCustomFields with (nolock)
+        WHERE InstanceCustomField = value
+        ),
+        value
+    )
+from
+    openjson(@DistributionFlagColumns)
+
+-------------------------------------------------------------------------------
+------------------------------------------------ End: Map Custom Field Names --
+-------------------------------------------------------------------------------
+
 select
     Domain,
     DocumentID,
@@ -131,13 +188,34 @@ from
             end,
         RevisionCreated = R.Created,
         DistChanges = (
-            select string_agg('( ' + CONVERT(NVARCHAR(30), Created, 20) + ': ' + FieldName + ' )', ' | ') from (
+            select string_agg(
+                cast(
+                    '( ' +
+                    convert(nvarchar(10), DocLogCreated, 120) +
+                    ' ' +
+                    CustomFieldName +
+                    ' : ' +
+                    isnull(DocLogOldValue, 'NULL') +
+                    ' > ' +
+                    isnull(DocLogFieldValue, 'NULL') +
+                    ' )'
+                    as nvarchar(max)
+                )
+                , ''
+            ) within group (order by DocLogCreated)
+            from (
                 SELECT
-                    Created,
-                    FieldName,
-                    OldValue,
-                    FieldValue,
-                    CreatedBy
+                    CustomFieldName = coalesce(
+                        (
+                            select FieldName
+                            from @FieldNameMapping f
+                            where f.ColumnName = DL.FieldName
+                        ),
+                        DL.FieldName),
+                    DocLogCreated = DL.Created,
+                    DocLogFieldName = DL.FieldName,
+                    DocLogOldValue = DL.OldValue,
+                    DocLogFieldValue = DL.FieldValue
                 FROM
                     dbo.atbl_DCS_DocumentsLog DL WITH (NOLOCK)
                 WHERE
