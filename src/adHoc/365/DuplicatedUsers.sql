34f0bb5cac947a2c39f11cda7d1564efdc537f41
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2025-09-19 16:57:14 +0200

    Update DuplicatedUsers.sql

--- src/adHoc/365/DuplicatedUsers.sql
+++ src/adHoc/365/DuplicatedUsers.sql
@@ -1,29 +1,68 @@
 /*
- * Duplicated users with row nubmer on created
+ * Duplicated users with row number, id and ADAccount
  */
 select
-    ID,
-    FirstName,
-    LastName,
-    Email,
-    RowNum = row_number() over (partition by FirstName, LastName, Email order by Created),
-    Created
+    P.ID,
+    P.FirstName,
+    P.LastName,
+    P.Email,
+    RowNum = row_number() over (
+        partition by
+            P.FirstName,
+            P.LastName,
+            P.Email
+        order by
+            P.Created),
+    P.Created,
+    P.ADAccount
+into
+    #DuplicatedPersons
 from
     dbo.stbl_System_Persons P
+    join dbo.stbl_System_PersonsUsers PU
+        on PU.Person_ID = P.ID
 where
     exists (
-        select FirstName,LastName,Email
-        from dbo.stbl_System_Persons Duplicates
+        select
+            D.FirstName,
+            D.LastName,
+            D.Email
+        from
+            dbo.stbl_System_Persons D -- Duplicates
         where
-            Duplicates.FirstName = P.FirstName
-            and Duplicates.LastName = P.LastName
-            and Duplicates.Email = P.Email
+            D.FirstName = P.FirstName
+            and D.LastName = P.LastName
+            and D.Email = P.Email
         group by
-            FirstName,
-            LastName,
-            Email
+            D.FirstName,
+            D.LastName,
+            D.Email
         having
             count(*) > 1
     )
 
--- select FirstName,LastName,Email from dbo.stbl_System_Persons group by FirstName,LastName,Email having count(*) > 1
+select * from #DuplicatedPersons
+
+
+
+/*
+ * Find duplicates
+ */
+-- select
+--     Count = count(*),
+--     FirstName,
+--     LastName,
+--     Email
+-- from
+--     dbo.stbl_System_Persons
+-- group by
+--     FirstName,
+--     LastName,
+--     Email
+-- having
+--     count(*) > 1
+-- order by
+--     Count desc,
+--     FirstName,
+--     LastName,
+--     Email
