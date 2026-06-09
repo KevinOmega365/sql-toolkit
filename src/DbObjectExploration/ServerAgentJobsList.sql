d1e9bf72f7d0f3c9a223210f6d6f938157d92788
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2026-06-09 13:20:44 +0200

    Update ServerAgentJobsList.sql

--- src/DbObjectExploration/ServerAgentJobsList.sql
+++ src/DbObjectExploration/ServerAgentJobsList.sql
@@ -1,19 +1,26 @@
-SELECT 
-    j.job_id,
+DECLARE @NamePattern nvarchar(max) = '%'
+
+SELECT
+    -- j.job_id,
     j.name AS JobName,
     c.name AS Category,
-    s.step_id,
+    -- s.step_id,
     s.step_name,
     s.subsystem,
     s.command,
     s.database_name,
-    s.last_run_outcome,
-    s.last_run_duration,
+    -- s.last_run_outcome,
+    -- s.last_run_duration,
     s.last_run_date,
     s.last_run_time
-FROM msdb.dbo.sysjobs AS j
-LEFT JOIN msdb.dbo.syscategories AS c 
-    ON j.category_id = c.category_id
-LEFT JOIN msdb.dbo.sysjobsteps AS s 
-    ON j.job_id = s.job_id
-ORDER BY j.name, s.step_id;
+FROM
+    msdb.dbo.sysjobs AS j
+    LEFT JOIN msdb.dbo.syscategories AS c
+        ON j.category_id = c.category_id
+    LEFT JOIN msdb.dbo.sysjobsteps AS s
+        ON j.job_id = s.job_id
+WHERE
+    j.name LIKE @NamePattern
+ORDER BY
+    j.name,
+    s.step_id;
