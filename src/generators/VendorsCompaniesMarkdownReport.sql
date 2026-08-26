deb0833e5d8e65f517d0e9100b953f13c0e7ce9e
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2026-08-26 11:51:13 +0200

    Create VendorsCompaniesMarkdownReport.sql

---
+++ src/generators/VendorsCompaniesMarkdownReport.sql
@@ -0,0 +1,67 @@
+/*
+ * Markdown Report: Vendors - Companies
+ *   vendors - email domains - user counts - missing values
+ */
+ 
+declare @AllowOnlyCleanData bit = 0
+
+declare @crlf nchar(2) = CHAR(13)+CHAR(10)
+declare @MarkdownTitleAndDescription nvarchar(max) = '# Vendors - Companies' + @crlf + @crlf + 'vendors - email domains - user counts - missing values'
+declare @MarkdownTableHeader nvarchar(max) = '| vendor | UserCount | EmailomainUserCount |' + @crlf + '| --- | --- | --- |'
+
+select
+    MarkdownReport =
+        @MarkdownTitleAndDescription + @crlf + @crlf +
+        @MarkdownTableHeader + @crlf +
+        string_agg(MarkdownTableRow, @crlf) + @crlf
+from
+    (
+        select
+            MarkdownTableRow = '| ' + vendor + ' | ' + cast(UserCount as nvarchar(3)) + ' | ' + EmailomainUserCountMarkdown + ' |'
+        from
+            (
+                select
+                    vendor,
+                    UserCount = sum(VendorDomainCount),
+                    EmailomainUserCountMarkdown = string_agg(EmailDomain + ': ' + cast(VendorDomainCount as nvarchar(3)), '<br>')
+                from
+                    (
+                        select
+                            VendorDomainCount = count(*),
+                            vendor,
+                            EmailDomain
+                        from
+                            (
+                                select
+                                    vendor = isnull(vendor, 'VENDOR_IS_BLANK'),
+                                    EmailDomain = isnull(
+                                        right(
+                                            lower(company_email),
+                                            len(company_email) - charindex('@', company_email) + 1
+                                        ),
+                                        'USER_EMAIL_IS_BLANK'
+                                    )
+                                from
+                                    dbo.ltbl_Import_TIF_PersonsPositions as I with (nolock)
+                                where
+                                    @AllowOnlyCleanData = 0
+                                    or
+                                    company_email is not null
+                                    and vendor is not null
+                            ) U
+                        group by
+                            vendor,
+                            EmailDomain
+                    ) T
+                group by
+                    vendor
+            ) U
+    ) V
+
+/*
+ * distinct vendors
+ */
+select distinct vendor
+from dbo.ltbl_Import_TIF_PersonsPositions as I with (nolock)
+where vendor is not null
+order by vendor
