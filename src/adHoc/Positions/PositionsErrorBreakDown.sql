6864f5164824290e2fbe51d8405349e900bafaf5
KevinOmega365 <97727548+KevinOmega365@users.noreply.github.com>
2026-09-10 14:42:38 +0200

    Update PositionsErrorBreakDown.sql

--- src/adHoc/Positions/PositionsErrorBreakDown.sql
+++ src/adHoc/Positions/PositionsErrorBreakDown.sql
@@ -1,16 +1,18 @@
-
 /*
  * Positions Error Break Down
  */
 select
     Count = count(*),
     Status = INTEGR_REC_STATUS,
+    ErrorMessage = INTEGR_REC_ERROR,
     Trace = INTEGR_REC_TRACE
 from
     dbo.ltbl_Import_TIF_PersonsPositions as T with (nolock)
 group by
     INTEGR_REC_STATUS,
+    INTEGR_REC_ERROR,
     INTEGR_REC_TRACE
 order by
     INTEGR_REC_STATUS,
+    INTEGR_REC_ERROR,
     INTEGR_REC_TRACE
